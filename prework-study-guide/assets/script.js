dd2d6066a3e4a3eb4d57c93cbe6cf49a755bfa71
danawelsh <111257868+danawelsh@users.noreply.github.com>
2023-01-25 16:11:23 -0500

    added and edited js

--- prework-study-guide/assets/script.js
+++ prework-study-guide/assets/script.js
@@ -1,4 +1,10 @@
-var topic = "HTML";
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+
+for (var x = 0; x < topics.length; x++) {
+    console.log(topics[x])
+}
+
+/*
 if (topic === 'HTML') {
     console.log("Let's study HTML!");
 } else if (topic === 'Git') {
@@ -10,4 +16,18 @@ if (topic === 'HTML') {
 } else {
     console.log('Please try again!');
 }
+*/
+
+/*
+//example of an array; each item has its own index [0],[1], so on
+var shapes = ["triangle", "square", "pentagon", "circle"];
+//console.log(shapes[0]);
+//console.log(shapes[1]);
+//console.log(shapes[2]);
+//console.log(shapes[3]);
+for(var x = 0; x < shapes.length; x++) {
+    console.log(shapes[x]);
+}
+*/
+
 
